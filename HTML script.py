d76e999a4a10a316ace865c36d66482c06c09001
syedtabasum <98376504+syedtabasum@users.noreply.github.com>
2022-02-14 16:32:33 +0530

    Add files via upload

---
+++ HTML script.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 15:04:30 2022
+
+@author: sytabasu
+"""
+
+from json2html import *
+import json
+import webbrowser
+import os
+with open('test_status.json', 'r') as f:
+  data = json.load(f)
+
+        
+
+file=json2html.convert(json = data)
+f = open('GFG.html', 'w')
+f.write(file)
+  
+# close the file
+f.close()
